464f3cba92956f779b55f0a607603e70d529e989
vincentvetri <vincentjeni93@gmail.com>
2022-01-21 13:29:18 +0530

    vincent task done

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee {
+	public void add() {
+		System.out.println("add");
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.add();
+	
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Vincentjava {
+}
